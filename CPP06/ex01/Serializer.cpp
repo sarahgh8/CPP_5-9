f6514239724a9b25472c79d412642bce667a15c0
sarahjgh8 <sarahjgh05@gmail.com>
2026-02-10 20:31:48 +0300

    chore(ex01): ex01 is done

---
+++ CPP06/ex01/Serializer.cpp
@@ -0,0 +1,17 @@
+#include "Serializer.hpp"
+
+uintptr_t Serializer::serialize(Data* ptr)
+{
+    uintptr_t res;
+
+    res = reinterpret_cast<uintptr_t>(ptr);
+    return res;
+}
+
+Data* Serializer::deserialize(uintptr_t raw)
+{
+    Data *res;
+
+    res = reinterpret_cast<Data *>(raw);
+    return res;
+}
